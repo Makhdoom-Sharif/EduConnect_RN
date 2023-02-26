be76ece2d05f27521c610a92816644049200ce4c
Makhdoom-Sharif <makhdomsharif@gmail.com>
2023-02-26 21:45:13 +0500

    login api WIP

--- src/Services/auth.jsx
+++ src/Services/auth.jsx
@@ -1,6 +1,11 @@
-import {registerUserCall} from '../backenAPICalls/authAPICall';
+import {loginUserCall, registerUserCall} from '../backenAPICalls/authAPICall';
 
 export const signUpUser = async props => {
   const {email, role, contactNo, password, username} = props;
   return await registerUserCall({email, role, contactNo, password, username});
+};
+
+export const loginUser = async props => {
+  const {email, role, contactNo, password, username} = props;
+  return await loginUserCall({email, role, contactNo, password, username});
 };
--- src/backenAPICalls/authAPICall.js
+++ src/backenAPICalls/authAPICall.js
@@ -8,3 +8,12 @@ export const registerUserCall = async props => {
     data: {email, role, contactNo, password, username},
   });
 };
+
+export const loginUserCall = async props => {
+  const {email, role, contactNo, password, username} = props;
+  return await axios({
+    method: 'post',
+    url: 'https://educonnectbackend-production.up.railway.app/api/auth/login',
+    data: {email, role, contactNo, password, username},
+  });
+};
